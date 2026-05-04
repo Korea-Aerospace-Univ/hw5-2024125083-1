4e001cfcdd1b36bf2ceaba8606cf535113c65918
2024125083 <choihyunjae1224@gmail.com>
2026-05-04 15:35:25 +0900

    Update main.c

--- p1/main.c
+++ p1/main.c
@@ -10,21 +10,20 @@ int main(void)
     int maxB = n / 750;
     int maxC = n / 200;
     
-    if(n < 1850)
-        printf("none");
-    
-    int a,b,c;
+    int found = 0;
     
     for(int a = 1; a <= maxA; a++){
         for(int b = 1; a <= maxB; b++){
             for(int c = 1; c <= maxC; c++){
                 if( (a*900 + b*750 + c*200) == n && ((c<a)||(c<b)) && b%2 == 0 ){
                     printf("%d %d %d\n",a,b,c);
-                }else{
-                    printf("none");
                 }
             }
         }
+    }
+
+    if(!found){
+        printf("none");
     }
     
 
